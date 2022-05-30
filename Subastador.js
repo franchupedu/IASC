208710da8e14a6639d6ee81ed3ffed3b2ceba082
hernan-arga <hernan_rodriguez_2013@hotmail.com>
2022-05-30 17:10:55 -0300

    Archivos iniciales

---
+++ Subastador.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+// for parsing the body in POST request
+var bodyParser = require('body-parser');
+
+var buyers =[];
+var bids =[];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+/* POST /buyers
+    {
+        "buyer": {
+            "id": 3,
+            "name": "Test User",
+            "ip" : "192.180.55.66",
+            "tags": ["Inmuebles", "Jueguitos"]
+        }
+    }
+*/
+app.post('/buyers', function (req, res) {
+    console.log(req.body);
+    var buyer = req.body.buyer;
+    buyers.push(buyer);
+
+    return res.send('Comprador '+buyer+' agregado exitosamente');
+});
+
+/* POST /bids
+    {
+        "bid": {
+            "id": 3,
+            "tags": ["Inmuebles", "Jueguitos"],
+            "price" : 92.5,
+            "duration": 50,
+            "article": {
+                //Detalles del articulo que no sabemos
+            }
+            //Al inicio es null
+            "buyer_ip": null
+        }
+    }
+*/
+app.post('/bids', function (req, res) {
+    var bid = req.body.bid;
+    bids.push(bid);
+
+    return res.send('Subasta numero '+bid.id+' agregada exitosamente');
+});
+
+/* POST /bid/:id
+    {
+        "offer": {
+            "buyer_ip": "172.550.25.25"
+            "price" : 150,
+        }
+    }
+*/
+app.post('/bid/:id', function (req, res) {
+    var offer = req.body.offer;
+    var id = req.params.id;
+    //Manejar que pasa si no es correcto el Id usando CPS
+    var bid = bids.find(b => b.id = id);
+    if(bid.price < offer.price){
+        bid.price = offer.price;
+        bid.buyer_ip = offer.buyer_ip;
+        //Notificar a todos
+        return res.send('La subasta '+bid.id+' tiene un nuevo precio de '+bid.price);
+    }
+    
+    //Notificar al mismo
+    return res.send('Tu precio fue demasiado bajo');
+});
+
+
+app.listen('8000', function(){
+    console.log('Server listening on port 8000');
+});
